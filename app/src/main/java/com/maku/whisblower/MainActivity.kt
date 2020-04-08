4a6bcda89509517137637e2d601f60cff6abc29b
MAZAKPE <makpalyy@gmail.com>
2020-04-08 21:21:35 +0300

    added utils files

--- app/src/main/java/com/maku/whisblower/MainActivity.kt
+++ app/src/main/java/com/maku/whisblower/MainActivity.kt
@@ -1,12 +1,91 @@
 package com.maku.whisblower
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat.animate
+import androidx.lifecycle.Observer
+import com.maku.whisblower.databinding.ActivityMainBinding
+import com.maku.whisblower.utils.NetworkUtils
+import com.maku.whisblower.utils.getColorRes
+import com.maku.whisblower.utils.hide
+import com.maku.whisblower.utils.show
+import com.shreyaspatil.MaterialDialog.MaterialDialog
 
 class MainActivity : AppCompatActivity() {
 
+    //databinding
+    private lateinit var mViewBinding: ActivityMainBinding
+
+    val mContext: Context =
+        WhisBlower.applicationContext()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme)  // Set AppTheme before setting content view.
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //handle newtwork changes
+        handleNetworkChanges()
+    }
+
+    /**
+     * Observe network changes i.e. Internet Connectivity
+     */
+    private fun handleNetworkChanges() {
+        NetworkUtils.getNetworkLiveData(applicationContext).observe(this, Observer { isConnected ->
+            if (!isConnected) {
+
+                mViewBinding.textViewNetworkStatus.text = getString(R.string.text_no_connectivity)
+                mViewBinding.networkStatusLayout.apply {
+                    alpha = 0f
+                    show()
+                    setBackgroundColor(getColorRes(R.color.colorStatusNotConnected))
+                    animate()
+                        .alpha(1f)
+                        .setDuration(ANIMATION_DURATION)
+                        .setListener(null)
+                }
+            } else {
+                mViewBinding.textViewNetworkStatus.text = getString(R.string.text_connectivity)
+                mViewBinding.networkStatusLayout.apply {
+                    setBackgroundColor(getColorRes(R.color.colorStatusConnected))
+
+                    animate()
+                        .alpha(0f)
+                        .setStartDelay(ANIMATION_DURATION)
+                        .setDuration(ANIMATION_DURATION)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                hide()
+                            }
+                        })
+                }
+            }
+        })
+    }
+
+    // Handle backpressed
+    override fun onBackPressed() {
+        MaterialDialog.Builder(this)
+            .setTitle("Exit?")
+            .setMessage("Are you sure want to exit?")
+            .setPositiveButton("Yes") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+                super.onBackPressed()
+            }
+            .setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            .build()
+            .show()
+    }
+
+    companion object {
+        const val ANIMATION_DURATION = 1000.toLong()
     }
 }
---
+++ app/src/main/java/com/maku/whisblower/utils/ViewUtils.kt
@@ -0,0 +1,12 @@
+package com.maku.whisblower.utils
+
+import android.view.View
+
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
